c4216d8cfd829841c1ffb751da1d4fef195eae34
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-27 22:47:18 +0100

    Update Hero component styling and content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,12 @@
 const Hero = () => {
 
   return (
-    <div className="h-[100vh]">
-      <div className="flex items-center justify-center">
+    <div className="h-[100vh] flex justify-center items-start">
+      <div className="flex items-center flex-col justify-center gap-[40px] w-[1008px] h-[345px]">
         <h1 className="font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 z-10">
-          <span className="text-slate-400">I'm</span> Sefe Akinbeye
+          <span className="text-[#808080]">I'm</span> Sefe Akinbeye
         </h1>
-        
+        <p className="text-[#808080] text-[25px] font-semibold w-[729px]"> I'm a skilled frontend developer who has worked with ReactJS, JavaScript, CSS3, and HTML5. If you're in search of someone to build pixel perfect  pages from Figma Designs, I'm the one for you. Let's collaborate to create something extraordinary that will make a lasting impact!  </p>
       </div>
     </div>
   );
