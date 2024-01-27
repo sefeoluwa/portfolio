6bb33f459fc56dbe31d9b03a8f18f24b1e4df2fa
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-27 17:09:39 +0100

    added screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    fontFamily: {
-      'lovers': ['Lovers Quarrel', 'cursive'],
-      'saint': ['Mrs Saint Delafield', 'cursive'],
-    }
+    extend: {
+      fontFamily: {
+        'lovers': ['Lovers Quarrel', 'cursive'],
+        'saint': ['Mrs Saint Delafield', 'cursive'],
+      },
+      screens: {
+        xs: "450px",
+        sm: '640px',
+        md: "710px",
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1536px',
+      },
+    },
   },
   plugins: [],
 }
