e69d23d9937a315fd38069b6b018e0115519991f
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-27 22:23:22 +0100

    Refactor Hero component to include a responsive heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
 
-export default Hero
+  return (
+    <div className="relative">
+      <div className="h-100vh flex items-center justify-center relative">
+        <h1 className="font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 z-10">
+          I'm Sefe Akinbeye
+        </h1>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
