33ab07b0f76321109c802e9c8bb72226fe1efdb9
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-27 22:36:22 +0100

    Refactor Hero component styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@
 const Hero = () => {
 
   return (
-    <div className="relative">
-      <div className="h-100vh flex items-center justify-center relative">
+    <div className="h-[100vh]">
+      <div className="flex items-center justify-center">
         <h1 className="font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2 z-10">
-          I'm Sefe Akinbeye
+          <span className="text-slate-400">I'm</span> Sefe Akinbeye
         </h1>
         
       </div>
