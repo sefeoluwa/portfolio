012a8e9284038c0a4cc5685325afa9d325feaa23
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-28 17:32:07 +0100

    section skeleton

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+
+
 
 const GetInTouch = () => {
   return (
-    <div>GetInTouch</div>
+    <div>
+      <div className="">
+        <h2>Let's work together</h2>
+        <p>Want to discuss an opportunity to create something great?
+I’m ready when you are.</p>
+      </div>
+      <div className="">
+      <button className="resume bg-slate-500 py-[16px] px-[32px] flex items-center justify-center h-[55px] w-[200px] rounded-[9px] text-[18px] gap-[8px] font-semibold text-white">Get in touch</button>
+      </div>
+    </div>
   )
 }
 
