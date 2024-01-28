7f9cef420a87ef82d36d9651c08499085eb6c24e
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-28 17:46:48 +0100

    section skeleton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Logo } from '../assets'
+import { footlinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex justify-around'>
+      <div className="">
+        <div className="">
+          <Logo />
+          <p>Thanks for stopping by ッ</p>
+        </div>
+        <p>© 2024 Sefe Akinbeye. All Rights Reserved.</p>
+      </div>
+
+      <div className="flex">
+        {footlinks.map((link) => (
+          <div className="" key={link.name}>
+            <p>{link.name}</p>
+            <ul>
+              {link.links.map((links) => (
+                <li key={links.name}>{links.name}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
