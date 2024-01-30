1eb137715886b0737e0be10daec3219d53459053
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 17:42:29 +0100

    fixed sizing for large screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { footlinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div className='flex justify-around items-start flex-col xxl:h-[350px] max-w-[1514px] p-5 '>
+    <div className='flex flex-wrap justify-around items-start flex-col xl:w-[98%] w-[264px] p-5 mb-8 '>
       <div className="h-[1px] bg-[#808080] w-full xxl:ml-5"></div>
       <div className="flex xs:justify-start md:justify-around items-start my-12 w-full xs:flex-wrap-reverse md:flex-wrap">
         <div className="flex flex-col justify-between items-start gap-[93px] ">
@@ -14,9 +14,9 @@ const Footer = () => {
           <p className='text-[#808080] text-[14px]'>© 2024 Sefe Akinbeye. All Rights Reserved.</p>
       </div>
 
-      <div className="lg:flex xs:flex-wrap  lg:gap-[120px]" >
+      <div className="flex xs:flex-wrap gap-[70px]  lg:gap-[120px]" >
         {footlinks.map((link) => (
-          <div className="flex flex-col gap-[16px] justify-start items-start" key={link.name}>
+          <div className="flex flex-col lg:gap-[16px] justify-start items-start" key={link.name}>
             <p className='text-[16px] font-bold '>{link.name}</p>
             <ul>
               {link.links.map((links) => (
