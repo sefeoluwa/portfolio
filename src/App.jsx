6175986708626889086a4f9cecd3cc74959401e0
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 17:25:12 +0100

    removed cursor tracking feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,10 @@
-import { useEffect, useState } from "react";
 import { Navbar, Footer, GetInTouch, GetToKnow, Hero, Projects } from "./components"
 function App() {
 
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-    };
-
-    const colorSchemeMediaQuery = window.matchMedia('(prefers-color-scheme: light)');
-
-    const handleColorSchemeChange = () => {
-      setIsDarkMode(colorSchemeMediaQuery.matches);
-    };
-
-    document.addEventListener('mousemove', handleMouseMove);
-    colorSchemeMediaQuery.addEventListener('change', handleColorSchemeChange);
-
-    // Initial color scheme check
-    setIsDarkMode(colorSchemeMediaQuery.matches);
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      colorSchemeMediaQuery.removeEventListener('change', handleColorSchemeChange);
-    };
-  }, []);
 
   return (
-    <div className="main h-svh  p-5 ">
-      <div className="p-5 xl:ml-10 md:ml-3">
-        <div
-          className="absolute"
-          style={{
-            width: '90%',
-            background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, transparent 15%, ${
-              isDarkMode ? '#fff' : '#000'
-            } 20%)`,
-          }}
-          >
+    <div className="xl:p-5 p-2 ">
+        <div className="main p-3 bg-tertiary rounded-3xl">
             <Navbar />
             <div className="pt-[15%]">
               <Hero />
@@ -50,7 +15,6 @@ function App() {
             <Footer />
         </div>
       </div>
-    </div>
   )
 }
 
