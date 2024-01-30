03255a4a09f97f2041b31fd4fd59177b2222f719
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 20:40:00 +0100

    fixed element mobile alignments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { footlinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div className='flex flex-wrap justify-around items-start flex-col md:w-[98%] w-[264px] p-5 mb-8 '>
+    <div className='flex flex-wrap justify-around items-start flex-col md:w-[98%] w-[40vw] p-5 mb-8 '>
       <div className="h-[1px] bg-[#808080] md:w-full w-[80vw] xxl:ml-5"></div>
-      <div className="flex xs:justify-start md:justify-around items-start my-12 w-full xs:flex-wrap-reverse md:flex-wrap">
-        <div className="flex flex-col justify-between items-start gap-[93px] ">
+      <div className="flex xs:justify-start md:justify-around items-start my-12 w-[50vw]  md:w-full flex-wrap-reverse md:flex-wrap gap-10">
+        <div className="flex flex-col justify-between items-start gap-[50px] lg:gap-[93px] ">
           <div className="">
             <Logo />
             <p className='text-[#808080] text-[14px]'>Thanks for stopping by <span className='text-[22px]'>ッ</span> </p>
@@ -14,7 +14,7 @@ const Footer = () => {
           <p className='text-[#808080] text-[14px]'>© 2024 Sefe Akinbeye. All Rights Reserved.</p>
       </div>
 
-      <div className="flex xs:flex-wrap gap-[70px]  lg:gap-[120px]" >
+      <div className="flex flex-wrap gap-[70px]  lg:gap-[120px]" >
         {footlinks.map((link) => (
           <div className="flex flex-col lg:gap-[16px] justify-start items-start" key={link.name}>
             <p className='text-[16px] font-bold '>{link.name}</p>
