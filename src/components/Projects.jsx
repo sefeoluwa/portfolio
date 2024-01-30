2f308cb1b57a167417f9767b304af57e80a67abf
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 10:26:59 +0100

    styling and positioning for cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,5 @@
-import { projects } from "../constants"
-// import { FaGithub } from 'react-icons'
-// import { Link }from 'react-router-dom'
-
+import { projects } from "../constants";
+import { FaGithub, FaEye } from 'react-icons/fa';
 
 const Projects = () => {
   return (
@@ -9,20 +7,30 @@ const Projects = () => {
       <h2 className="font-bold text-center lg:text-[48px] sm:text-[35px] xs:text-[30px] text-[16px] lg:leading-[40px]">Selected Work</h2>
       <p className="flex items-center justify-center text-center w-[80%] mt-8 text-secondary text-[20px] max-w-3xl leading-[30px]"> The following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. They reflect my ability to solve complex problems, work with different technologies, and manage projects effectively.</p>
 
-      <div className="">
+      <div className="mt-20 pl-0 p-5 lg:pl-[13%] flex flex-wrap gap-12 items-center justify-start">
         {projects.map((project) => (
-          <div className="" key={project.name}>
-            <p>{project.name}</p>
-            <p>{project.description}</p>
+          <div className="relative rounded-2xl sm:w-[360px] w-full h-fit p-4" key={project.name}>
+            <img src={project.image} alt="" className="w-full h-[60%] rounded-2xl" />
+            <div className="flex gap-8 items-end m-3 card-img_hover z-[2]">
+              <button
+                onClick={() => window.open(project.live_preview_link, "_blank")}
+                className="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+                <FaEye style={{ fontSize: '30px' }}/>
+              </button>
+              <button
+                onClick={() => window.open(project.source_code_link, "_blank")}
+                className="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+                <FaGithub style={{ fontSize: '30px' }}/>
+              </button>
+            </div>
+            <p className="font-bold text-[24px]">{project.name}</p>
+            <p className="mt-2 text-[15px]">{project.description}</p>
             <p>{project.tags}</p>
-            <a href={project.live_preview_link}>Live Preview</a>
-            {/* <Link to={project.source_code_link}><FaGithub /></Link> */}
-            <img src={project.image} alt="" />
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
