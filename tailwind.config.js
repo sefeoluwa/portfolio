75db12ab3ccf7a118a1725cd5a9837ef3584d93a
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 11:47:36 +0100

    added bg-tertiary :tada:

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        tertiary: "#151030",
+      "black-100": "#100d25",
+      "black-200": "#090325",
+      "white-100": "#f3f3f3",
+
+      },
       fontFamily: {
         'lovers': ['Lovers Quarrel', 'cursive'],
         'saint': ['Mrs Saint Delafield', 'cursive'],
