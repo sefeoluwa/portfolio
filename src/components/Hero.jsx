6fa8a4835718ab7cfdf10c7a8538cd0e13e562cb
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 14:08:04 +0100

    added new button class and styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 
 
 const Hero = () => {
@@ -9,9 +10,9 @@ const Hero = () => {
           <span className="text-[#808080]">I'm</span> Sefe Akinbeye
         </h1>
         <p className="text-[25px] font-semibold w-[729px] "> I'm a skilled frontend developer who has worked with ReactJS, JavaScript, CSS3, and HTML5. If you're in search of someone to build pixel perfect  pages from Figma Designs, I'm the one for you. Let's collaborate to create something extraordinary that will make a lasting impact!  </p>
-        <div className="flex gap-[16px] ">
-          <button className="resume bg-slate-500 py-[16px] px-[32px] flex items-center justify-center h-[55px] w-[200px] rounded-[9px] text-[18px] gap-[8px] font-semibold text-white">See my resume</button>
-          <button className="touch-btn flex h-[55px] py-[16px] px-[32px] bg-[#181818] text-[#808080] rounded-[9px] text-[18px]">Get in touch</button>
+        <div className="flex gap-[16px] flex-wrap">
+          <button className="main-button  flex items-center justify-center font-semibold text-white">See my resume</button>
+          <button className="touch-btn flex h-[55px] py-[16px] px-[40px] bg-[#181818] rounded-[1.87rem] text-[18px]">Get in touch</button>
         </div>
       </div>
     </div>
