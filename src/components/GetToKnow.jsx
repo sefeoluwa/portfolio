4cc6420b5e796a4bb049643c77974df85a80d1d0
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 13:02:31 +0100

    font styles for card texts

--- src/components/GetToKnow.jsx
+++ src/components/GetToKnow.jsx
@@ -7,9 +7,9 @@ const GetToKnow = () => {
 
       <div className="flex flex-wrap items-start justify-around gap-[16px] w-full mt-[5%]">
         {know.map((about) => (
-          <div className="bg-[#44373766] flex flex-col items-center justify-center gap-5 rounded-[10px] h-[380px] w-[400px] overflow-hidden" key={about.name}>
-            <p>{about.name}</p>
-            <p>{about.description}</p>
+          <div className="bg-[#44373766] flex flex-col items-center justify-center gap-5 rounded-[32px] h-[380px] w-[400px] overflow-hidden" key={about.name}>
+            <p className='text-[32px] font-bold'>{about.name}</p>
+            <p className='text-[16px] text-[#808080]'>{about.description}</p>
             <div className="object-contain w-[380px] rounded-xl overflow-hidden">
             <img className='' src={about.image} alt="" />
             </div>
