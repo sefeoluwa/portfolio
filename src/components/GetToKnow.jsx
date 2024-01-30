74082ad4484e2c596e776f96e87507a78d9d9e29
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 12:59:05 +0100

    Refactor GetToKnow component layout and styles

--- src/components/GetToKnow.jsx
+++ src/components/GetToKnow.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
 import { know } from '../constants'
 
 const GetToKnow = () => {
   return (
-    <div>
-      <div className="">
+    <div className='flex flex-col gap-[32px] mt-[20vh] lg:h-[100vh] p-5'>
+      <h2 className="flex justify-center w-full font-bold text-center lg:text-[48px] sm:text-[35px] xs:text-[30px] text-[16px] lg:leading-[40px]">Get to Know me</h2>
+
+      <div className="flex flex-wrap items-start justify-around gap-[16px] w-full mt-[5%]">
         {know.map((about) => (
-          <div className="" key={about.name}>
+          <div className="bg-[#44373766] flex flex-col items-center justify-center gap-5 rounded-[10px] h-[380px] w-[400px] overflow-hidden" key={about.name}>
             <p>{about.name}</p>
             <p>{about.description}</p>
-            <img src={about.image} alt="" />
+            <div className="object-contain w-[380px] rounded-xl overflow-hidden">
+            <img className='' src={about.image} alt="" />
+            </div>
           </div>
         ))}
       </div>
