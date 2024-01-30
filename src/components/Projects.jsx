3aa64272d311bb97d96e871b2b695cff8b1ef1f0
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-30 18:08:54 +0100

    Fixed body text format

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="font-bold text-center lg:text-[48px] sm:text-[35px] xs:text-[30px] text-[32px] lg:leading-[40px]">Selected Work</h2>
-      <p className="flex items-center justify-center text-center w-[80%] mt-8 text-[20px] max-w-3xl leading-[30px]"> The following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. They reflect my ability to solve complex problems, work with different technologies, and manage projects effectively.</p>
+      <p className="flex items-center justify-center text-justify  w-[80%] mt-8 text-[20px] max-w-3xl leading-[30px]"> The following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. They reflect my ability to solve complex problems, work with different technologies, and manage projects effectively.</p>
 
       <div className="mt-20 pl-0 p-5 lg:pl-[13%] flex flex-wrap gap-12 items-center justify-center lg:justify-start">
         {projects.map((project) => (
