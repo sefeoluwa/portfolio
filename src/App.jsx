97ea560dac04a3ccfa8954574c2af922959a750b
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-31 13:09:53 +0100

    Update App.jsx styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <BrowserRouter>
     
-    <div className="xl:p-5 p-1 ">
-        <div className="main bg-tertiary rounded-3xl">
+    <div className=" flex items-center justify-center ">
+        <div className="main w-[95vw] bg-tertiary rounded-3xl">
             <Navbar />
             <div className="pt-[15%]">
               <Hero />
