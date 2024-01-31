b3f16204afa6704a5ee6449a3949b6f951c3812a
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-31 11:22:00 +0100

    added link to hero from logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,26 @@
+import { Link } from 'react-router-dom'
 import { Logo } from '../assets'
 import { footlinks } from '../constants'
+import { useState } from 'react';
 
 const Footer = () => {
+  const [active, setActive] = useState("");
+
   return (
     <div className='flex flex-wrap justify-around items-start flex-col md:w-[98%] w-[40vw] p-5 mb-8 '>
       <div className="h-[1px] bg-[#808080] md:w-full w-[80vw] xxl:ml-5"></div>
       <div className="flex xs:justify-start md:justify-around items-start my-12 w-[50vw]  md:w-full flex-wrap-reverse md:flex-wrap gap-10">
         <div className="flex flex-col justify-between items-start gap-[50px] lg:gap-[93px] ">
           <div className="">
+            <Link  
+            to='#hero' 
+          className="cursor-pointer"
+          onClick={() => {
+            setActive("");
+            document.getElementById('hero').scrollIntoView({ behavior: 'smooth' });
+          }}>
             <Logo />
+            </Link>
             <p className='text-[#808080] text-[14px]'>Thanks for stopping by <span className='text-[22px]'>ッ</span> </p>
           </div>
           <p className='text-[#808080] text-[14px]'>© 2024 Sefe Akinbeye. All Rights Reserved.</p>
