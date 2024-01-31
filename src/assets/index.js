4b596d3cec6df5dffd794aad3baf29098ed9b0b3
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-31 14:35:15 +0100

    Add new images and update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,8 +10,40 @@ import clone from './clone.png'
 import Tech from './Tech.png'
 import Bookshelf from './Bookshelf.png'
 import sefe from './sefe.jpeg'
+import atomic_habits from './atomic_habits.png'
+import Book from './Book.png'
+import css from './css.png'
+import greatestSalesman from './greatestSalesman.jpg'
+import harry from '/harry.png'
+import headfirstJS from './headfirstJS.jpg'
+import Improbable from './Improbable.jpg'
+import jsVisually from './jsVisually.jpg'
+import novice from './novice.jpg'
+import secrets from './secrets.png'
+import smarter from './smarter.jpg'
+import steal from './steal.png'
+import vulture from './vulture.jpg'
+import eloquent from './eloquent.jpg'
+import modern from './modern.jpg'
+import data from './data.jpg'
 
 export {
+    atomic_habits,
+    novice,
+    Book,
+    css,
+    greatestSalesman,
+    harry,
+    headfirstJS,
+    Improbable,
+    jsVisually,
+    secrets,
+    smarter,
+    steal,
+    vulture,
+    data,
+    modern,
+    eloquent,
     Logo,
     globe,
     menu_app,
