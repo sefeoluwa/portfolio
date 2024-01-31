085579a990fee2e1681202cf9fe35c7c3c1b5c4e
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-31 15:57:38 +0100

    Update Bookshelf page layout and content

--- src/pages/Bookshelf.jsx
+++ src/pages/Bookshelf.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+/* eslint-disable react/no-unescaped-entities */
+import { shelf } from '../constants'
 const Bookshelf = () => {
   return (
-    <div>Bookshelf</div>
+   <div className="py-40 overflow-hidden">
+<div className="flex flex-col justify-center px-[18.5%]">
+<h1 className='font-black lg:text-[80px] sm:text-[60px] xs:text-[48px] text-[38px] lg:leading-[98px] mt-7 '>Bookshelf</h1>
+    <p className="text-[16px] md:text-[24px] w-[358px] md:w-[729px] text-[#808080]">Books and pieces of wisdom I've enjoyed reading.</p>
+</div>
+    <div className="flex justify-center">
+    <div className='flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-center lg:grid grid-cols-4 place-items-center mt-10'>
+      {shelf.map((shelf, index) => (
+        <div className="max-w-[215px]" key={index}>
+          <img src={shelf} alt="" />
+        </div>
+      ))}
+    </div>
+    </div>
+   </div>
   )
 }
 
