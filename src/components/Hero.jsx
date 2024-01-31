bd2c0f0ebd39bec54676426f4566cde844218da5
sefeoluwa <sefeoluwa01@gmail.com>
2024-01-31 14:09:37 +0100

    Add link to resume and update button styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,9 @@ const Hero = () => {
         </h1>
         <p className="text-[15px] md:text-[25px] font-semibold w-[358px] md:w-[729px] text-[#808080] px-2"> A skilled frontend developer who has worked with ReactJS, JavaScript, CSS3, and HTML5. If you're in search of someone to build pixel perfect  pages from Figma Designs, I'm the one for you. Let's collaborate to create something extraordinary that will make a lasting impact!  </p>
         <div className="flex gap-[16px] flex-wrap justify-center md:justify-normal">
+          <a href="https://flowcv.com/resume/ok647of1rh" target="blank" rel="noreferrer">
           <button className="main-button  flex items-center justify-center font-semibold text-white">See my resume</button>
+          </a>
           <button className="touch-btn flex h-[55px] py-[16px] px-[40px] bg-[#181818] rounded-[1.87rem] text-[18px]">Get in touch</button>
         </div>
       </div>
