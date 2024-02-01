c9a924e961a7ea92fd21bf195b75052efbe7526a
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-01 23:24:19 +0100

    changed bg for project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,10 +7,10 @@ const Projects = () => {
       <h2 className="font-bold text-center lg:text-[48px] sm:text-[35px] xs:text-[30px] text-[32px] lg:leading-[40px]">Selected Work</h2>
       <p className="flex items-center justify-center text-justify  w-[80%] mt-8 text-[20px] max-w-3xl leading-[30px]"> The following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. They reflect my ability to solve complex problems, work with different technologies, and manage projects effectively.</p>
 
-      <div className="mt-20 pl-0 p-5 lg:pl-[2%] xxl:pl-[13%] flex flex-wrap gap-12 items-center justify-center lg:justify-start">
+      <div className="mt-20 pl-0 p-5 md:pl-[2%] xxl:pl-[13%] flex flex-wrap gap-12 items-center justify-center md:justify-start">
         {projects.map((project) => (
           <div className="project-card  py-[3px]  rounded-[15px]" key={project.name}>
-            <div className="relative rounded-2xl w-[330px] lg:w-[350px] lg:h-[550px] h-[500px] p-4 bg-tertiary " >
+            <div className="relative rounded-2xl w-[330px] lg:w-[350px] lg:h-[550px] h-[500px] p-4 bg-[#191938]" >
             <img src={project.image} alt="" className="w-full rounded-2xl object-cover" />
             <div className="flex gap-8 items-end m-3 card-img_hover z-[2]">
               <button
