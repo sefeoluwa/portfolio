1b3482cc9509206ce68a5e924eaee6f283404aa3
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-01 19:29:46 +0100

    base alignment for icons

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -2,19 +2,19 @@ import { tech } from "../constants"
 
 const Stack = () => {
   return (
-    <div>
+    <div className="py-20 px-16 overflow-hidden">
       <div className="">
-        <h2>Tech Stack</h2>
-        <p>The dev tools and apps I use.</p>
+        <h2 className="font-black lg:text-[80px] sm:text-[60px] text-[38px] lg:leading-[98px] mt-7">Tech Stack</h2>
+        <p className="text-[18px]">The dev tools and apps I use.</p>
       </div>
 
      <div className="">
         <div className="">
             <h2>Dev & Design</h2>
-            <div className="">
+            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-start md:grid grid-cols-4 place-items-center mt-10">
               {tech.dev.map((dev) => (
                 <div className="" key={dev.name}>
-                  <img src={dev.logo} alt="" />
+                  <img src={dev.logo} alt="" className="max-w-[215px]"/>
                   <div className="">
                     <p>{dev.name}</p>
                     <p>{dev.description}</p>
@@ -26,10 +26,10 @@ const Stack = () => {
 
           <div className="">
             <h2>Apps</h2>
-            <div className="">
+            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-start md:grid grid-cols-4 place-items-center mt-10">
           {tech.app.map((app) => (
             <div className="" key={app.name}>
-              <img src={app.logo} alt="" className=""/>
+              <img src={app.logo} alt="" className="max-w-[215px]"/>
               <div className="">
                 <p>{app.name}</p>
                 <p>{app.description}</p>
