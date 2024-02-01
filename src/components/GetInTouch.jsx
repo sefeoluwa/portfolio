aced56af4150cf12b22081358ae974c313f18a70
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-01 23:50:37 +0100

    fix: removed pushed bug

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -11,11 +11,8 @@ const GetInTouch = () => {
         <p className="flex justify-start  mt-8 text-[20px] max-w-[496px] leading-[30px] text-[#808080]">Want to discuss an opportunity to create something great? I’m ready when you are!</p>
       </div>
       
-<<<<<<< HEAD
       <Link to="/contact" className="main-button py-[16px] px-[32px] flex items-center justify-center h-[55px] rounded-[9px]  gap-[8px] font-semibold text-white"><FaRocket /> <button >Get in touch</button></Link>
-=======
       <button className="main-button py-[16px] px-[32px] flex items-center justify-center h-[55px] rounded-[9px]  gap-[8px] font-semibold text-white"><FaRocket /> <Link to="/contact">Get in touch</Link></button>
->>>>>>> 9101acca36c491fcf8b71a4025e5c25352aa536d
       </section>
   )
 }
