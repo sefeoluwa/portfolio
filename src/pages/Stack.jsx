01c79c6c10de0cb82d5c6911ea206f4a31063914
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-01 20:54:57 +0100

    fixed font and style  for tech description

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -10,14 +10,14 @@ const Stack = () => {
 
      <div className="">
         <div className="">
-            <h2>Dev & Design</h2>
-            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-between mt-10 lg:justify-start">
+            <h2 className="mt-7 text-[20px] font-bold  md:text-[30px]">Dev & Design</h2>
+            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-between mt-5 lg:justify-start">
           {tech.dev.map((dev) => (
-            <div className="tech p-10 flex flex-col items-center justify-center w-[280px] max-h-[250px] gap-5 rounded-2xl " key={dev.name}>
+            <div className="tech p-10 flex flex-col items-center justify-center w-[310px] max-h-[280px] gap-5 rounded-2xl " key={dev.name}>
               <img src={dev.logo} alt="" className="w-[150px]"/>
-              <div className="">
-                <p>{dev.name}</p>
-                <p>{dev.description}</p>
+              <div className="tech-desc-container flex justify-between w-[120%] font-semibold py-5">
+                <p className="flex items-center">{dev.name}</p>
+                <p className="tech-desc w-[65%] flex justify-center items-center p-2 rounded-3xl text-[15px] font-normal">{dev.description}</p>
               </div>
             </div>
           ))}
@@ -25,14 +25,14 @@ const Stack = () => {
           </div>
 
           <div className="">
-            <h2>Apps</h2>
+            <h2 className="mt-7 md:text-[30px] text-[20px] font-bold">Apps</h2>
             <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-between mt-10 lg:justify-start">
           {tech.app.map((app) => (
             <div className="tech p-10 flex flex-col items-center justify-center w-[280px] max-h-[250px] rounded-2xl" key={app.name}>
               <img src={app.logo} alt="" className="w-[150px]"/>
-              <div className="">
-                <p>{app.name}</p>
-                <p>{app.description}</p>
+              <div className="flex justify-between w-[120%] font-semibold py-5">
+                <p className="flex items-center">{app.name}</p>
+                <p className="tech-desc w-[65%] flex justify-center items-center p-2 rounded-3xl text-[15px] font-normal">{app.description}</p>
               </div>
             </div>
           ))}
