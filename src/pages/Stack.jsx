9f54fee1b623ad615c35597bff92e197f212ace4
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-01 20:17:39 +0100

    alignment for tech cards

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -2,7 +2,7 @@ import { tech } from "../constants"
 
 const Stack = () => {
   return (
-    <div className="py-20 px-16 overflow-hidden">
+    <div className="py-20 px-16 overflow-hidden lg:w-[90%]">
       <div className="">
         <h2 className="font-black lg:text-[80px] sm:text-[60px] text-[38px] lg:leading-[98px] mt-7">Tech Stack</h2>
         <p className="text-[18px]">The dev tools and apps I use.</p>
@@ -11,9 +11,9 @@ const Stack = () => {
      <div className="">
         <div className="">
             <h2>Dev & Design</h2>
-            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-between mt-10">
+            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-between mt-10 lg:justify-start">
           {tech.dev.map((dev) => (
-            <div className="tech p-10 flex flex-col items-center justify-center w-[280px] max-h-[250px] gap-5" key={dev.name}>
+            <div className="tech p-10 flex flex-col items-center justify-center w-[280px] max-h-[250px] gap-5 rounded-2xl " key={dev.name}>
               <img src={dev.logo} alt="" className="w-[150px]"/>
               <div className="">
                 <p>{dev.name}</p>
@@ -26,9 +26,9 @@ const Stack = () => {
 
           <div className="">
             <h2>Apps</h2>
-            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-between mt-10">
+            <div className="flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-between mt-10 lg:justify-start">
           {tech.app.map((app) => (
-            <div className="tech p-10 flex flex-col items-center justify-center w-[280px] max-h-[250px]" key={app.name}>
+            <div className="tech p-10 flex flex-col items-center justify-center w-[280px] max-h-[250px] rounded-2xl" key={app.name}>
               <img src={app.logo} alt="" className="w-[150px]"/>
               <div className="">
                 <p>{app.name}</p>
