ea3cf55a1ac25f972f2f9425c0e672e4579f8a0e
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-01 13:43:30 +0100

    Add new tech icons

---
+++ src/assets/tech/index.js
@@ -0,0 +1,54 @@
+import css from "./css.png";
+import figma from "./figma.png";
+import Github from "./Github.png";
+import html from "./html.png";
+import javascript from "./javascript.png";
+import nodejs from "./nodejs.png";
+import reactjs from "./reactjs.png";
+import redux from "./redux.png";
+import tailwind from "./tailwind.png";
+import threejs from "./threejs.svg";
+import chatgpt from './chatgpt.png'
+import discord from './discord.png'
+import duolingo from './duolingo.png'
+import firebase from './firebase.png'
+import framer from './framer.png'
+import grapql from './graphql.png'
+import next from './next.png'
+import notion from './notion.png'
+import slack from './slack.png'
+import ts from './ts.png'
+import vercel from './vercel.png'
+import vscode from './vscode.png'
+import clickup from './clickup.png'
+import trello from './trello.png'
+
+
+
+export{
+    css,
+    figma,
+    Github,
+    html,
+    javascript,
+    nodejs,
+    reactjs,
+    redux,
+    tailwind,
+    threejs,
+    chatgpt,
+    discord,
+    duolingo,
+    firebase,
+    framer,
+    grapql,
+    next,
+    slack,
+    notion,
+    ts,
+    trello,
+    vercel,
+    vscode,
+    clickup,
+
+}
