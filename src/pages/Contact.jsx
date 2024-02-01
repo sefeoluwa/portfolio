57e1c8fb1fdcbefddf3bacf6b5e27e32d65a3e14
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-01 12:34:15 +0100

    Refactor Contact page layout and styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -69,14 +69,16 @@ const Contact = () => {
   };
 
   return (
-    <div className='h-[70vh] flex items-center justify-center p-10'>
-      <div  className='flex flex-col items-start justify-center form md:w-[800px] md:h-690px h-[684px] w-[358px]' >
-        <h1 className='font-black lg:text-[80px] sm:text-[60px] text-[38px] lg:leading-[98px] mt-7 '>Get in touch</h1>
-        <p className='font-bold'>Let's build something awesome!</p>
-
+    <div className='h-[90vh] flex items-center justify-center p-10 overflow-hidden'>
+      <div  className='flex flex-col items-start justify-center form md:w-[800px] md:h-690px h-[684px] w-[358px] gap-12' >
        <div className="">
+       <h1 className='font-black lg:text-[80px] sm:text-[60px] text-[38px] lg:leading-[98px] mt-7 '>Get in touch</h1>
+        <p className='text-[18px]'>Let's build something awesome!</p>
+       </div>
 
-          <div className="flex gap-60 p-4 items-center">
+       <div className="border-[0.3px] border-[#808080] rounded-2xl px-10 w-full">
+
+          <div className="flex gap-60 p-4 items-center border-b-[0.3px] border-[#808080] w-full">
             <div className="flex gap-2">
               <div className="w-3 h-3 bg-[#F63636] rounded-full"></div>
               <div className="w-3 h-3 bg-[#F6C136] rounded-full"></div>
@@ -85,14 +87,16 @@ const Contact = () => {
             <p>New Message</p>
           </div>
 
-          <form ref={formRef} onSubmit={handleSubmit} className=''>
-          <label htmlFor="name" className="">
-            <span className=''>Your Name</span>
-            <input type="text" name='name' id='name' value={form.name} onChange={handleChange} placeholder="What's your name?" required className='' aria-label='name'/>
+          <form ref={formRef} onSubmit={handleSubmit} className='mt-6 flex flex-col gap-10 w-full justify-center'>
+          <label htmlFor="name" className="flex flex-wrap gap-5 ">
+            <span className=''>Your Name:</span>
+            <input type="text" name='name' id='name' value={form.name} onChange={handleChange} placeholder="What's your name?" required 
+            className='outline-none border-b-[0.5px] border-[#808080]'  
+            aria-label='name'/>
           </label>
 
-          <label htmlFor="email" className="">
-                <span className=''>Your Email</span>
+          <label htmlFor="email" className="flex flex-wrap items-center gap-5">
+                <span className=''>Your Email:</span>
                 <input
                   type="text"
                   name='email'
@@ -101,7 +105,7 @@ const Contact = () => {
                   onChange={handleChange}
                   placeholder="Your email"
                   required
-                  className={`placeholder:text-secondary rounded-lg outlined-none border-none font-medium ${!isValidEmail ? 'border-red-500' : ''}`}
+                  className={`placeholder:text-[#808080] outline-none  border-b-[0.5px] border-[#808080] p-2 font-medium ${!isValidEmail ? 'border-red-500' : ''}`}
                 />
                 {!isValidEmail && (
                   <span className="text-red-500">Invalid email format</span>
@@ -109,8 +113,8 @@ const Contact = () => {
               </label>
 
           <label htmlFor="" className="flex flex-col">
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea rows='7' name='message' value={form.message} onChange={handleChange} placeholder="What do you want to say?" required className='placeholder:text-secondary ' />
+            <span className=' font-medium mb-4'>Your Message:</span>
+            <textarea rows='7' name='message' value={form.message} onChange={handleChange} placeholder="Write your message here" required className='placeholder:text-[#808080] bg-tranparent outline-none p-4  border-[0.5px] border-[#808080]' />
           </label>
 
             <button type='submit' className=''>{loading ? 'Sending...' : sendButton}</button>
