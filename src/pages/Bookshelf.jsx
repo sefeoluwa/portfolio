3f8213fc773d152c95582e36160567313262e903
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-02 08:20:35 +0100

    Update Bookshelf.jsx with max-width for text element

--- src/pages/Bookshelf.jsx
+++ src/pages/Bookshelf.jsx
@@ -5,7 +5,7 @@ const Bookshelf = () => {
    <div className="py-40 overflow-hidden">
 <div className="flex flex-col justify-center px-[18.5%]">
 <h1 className='font-black lg:text-[80px] sm:text-[60px] xs:text-[48px] text-[38px] lg:leading-[98px] mt-7 '>Bookshelf</h1>
-    <p className="text-[16px] md:text-[24px] w-[358px] md:w-[729px] text-[#808080]">Books and pieces of wisdom I've enjoyed reading.</p>
+    <p className="text-[16px] md:text-[24px] max-w-[358px] md:w-[729px] text-[#808080]">Books and pieces of wisdom I've enjoyed reading.</p>
 </div>
     <div className="flex justify-center">
     <div className='flex flex-wrap xxl:gap-[60px] gap-[24px] items-center justify-center lg:grid grid-cols-4 place-items-center mt-10'>
