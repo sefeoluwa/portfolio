22a02ef2c6afba11b4feae0367c96bd7e0334073
sefeoluwa <sefeoluwa01@gmail.com>
2024-02-05 12:31:54 +0100

    Add meta tags and image for SEO

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from 'react';
 import { GetInTouch, GetToKnow, Hero, Projects } from '../components'
+import { Helmet } from 'react-helmet';
+import { sefe } from '../assets';
+
 
 const Home = () => {
   useEffect(() => {
@@ -7,13 +10,21 @@ const Home = () => {
   }, []); 
 
   return (
+    <>
+    <Helmet>
+        <title>Sefeoluwa Akinbeye</title>
+        <meta name="description" content="Skilled frontend developer with 2+ years of development experience. Proficient in ReactJS, JavaScript, NextJS, TypeScript, CSS3, and TailwindCSS. Let's collaborate to create something extraordinary that will make a lasting impact!" />
+        <meta property="og:image" content={sefe} />
+      </Helmet>
     <div className="pt-[8%] mx-3 md:mx-0 w-[90vw]">
               <Hero />
               <Projects />
               <GetToKnow />
               <GetInTouch />
             </div>
+    </>
   )
 }
 
 export default Home
+
