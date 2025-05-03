408ae944ce257109dd3aa3e2b2902625ce4fe894
Sefeoluwa Akinbeye <sefeoluwa01@gmail.com>
2025-05-03 13:16:47 +0100

    Reverted to old content

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -2,8 +2,35 @@ import Blog from "./Blog";
 import Startup1 from "./Startup1";
 import Startup2 from "./Startup2";
 import IP1 from "./IP1";
+import spotify from "../../assets/tech"
+import startup2 from "./Startup2";
 
+const article = [
+    {
+        name: "IP1",
+        id: "IP1",
+        description: "IP IP IP IP IP IP IP IP",
+        link: "./IP1",
+        metaImg: spotify
 
+    },
+    {
+        name: "Startup1",
+        id: "startup1",
+        description: "IP IP IP IP IP IP IP IP",
+        link: Startup1,
+        metaImg: spotify
+
+    },
+    {
+        name: "Startup2",
+        id: "startup2",
+        description: "IP IP IP IP IP IP IP IP",
+        link: startup2,
+        metaImg: spotify
+
+    },
+]
 
 
 export {
@@ -11,4 +38,5 @@ export {
     Startup1,
     Startup2,
     IP1,
+    article,
 }
